6dae13d60045124b582003bf5788c79c1b38a037
jaankaup <janne.a.kauppinen@student.jyu.fi>
2020-12-03 14:12:01 +0200

    Event loop created.

--- hello_project/src/main.rs
+++ hello_project/src/main.rs
@@ -1,13 +1,63 @@
-use jaankaup_core::wgpu_system;
-//#[path = "../../wgpu_system.rs"]
-//mod wgpu_system;
-//
-//#[path = "../../application.rs"]
-//mod application;
+#[cfg(target_arch = "wasm32")]
+use futures::task::LocalSpawn;
 
-/// A struct for features and limits used in this application.
+use jaankaup_core::wgpu_system as ws;
+
 struct MyFeatures {}
-impl wgpu_system::WGPUFeatures for MyFeatures { }
+impl ws::WGPUFeatures for MyFeatures { }
+
+struct Hello_app {
+}
+
+impl ws::Application for Hello_app  {
+
+    fn init(self, configuration: &ws::WGPUConfiguration) -> Self {
+        Hello_app {
+        }
+    }
+
+    fn render(self) {
+
+    }
+
+    fn input(self) {
+
+    }
+
+    fn resize(self) {
+
+    }
+
+    fn update(self) {
+
+    }
+
+}
+
+// pub trait Application: Sized + 'static {
+// 
+//     /// Creates an Application.
+//     fn init(self, configuration: &WGPUConfiguration) -> Self;
+// 
+//     /// The render function for application.
+//     fn render(self);
+// 
+//     /// A function that handles inputs.
+//     fn input(self);
+// 
+//     /// A function for resizing.
+//     fn resize(self);
+// 
+//     /// A function for updating the state of the application.
+//     fn update(self);
+// }
+
+// /// A trait for Loops.
+// pub trait Loop: Sized + 'static {
+// 
+//     /// Run function that starts the loop.
+//     fn run<A: Application>(self, configuration: WGPUConfiguration);
+// }
 
 struct TestApp {
 
@@ -16,6 +66,8 @@ struct TestApp {
 fn main() {
     
     println!("Started...");
-    let jaahans = futures::executor::block_on(wgpu_system::setup::<MyFeatures>("jihuu"));
+    let configuration = futures::executor::block_on(ws::setup::<MyFeatures>("jihuu")).expect("Failed to create WGPUConfiguration.");
+
+
     println!("Finished...");
 }
--- jaankaup_core/src/wgpu_system.rs
+++ jaankaup_core/src/wgpu_system.rs
@@ -1,22 +1,158 @@
+#[cfg(target_arch = "wasm32")]
+use futures::task::LocalSpawn;
+
 use winit::{
     event::{Event, WindowEvent,KeyboardInput,ElementState,VirtualKeyCode},
     event_loop::{ControlFlow, EventLoop},
     window::Window
 };
 
+/// A trait for wgpu-rs based application.
 pub trait Application: Sized + 'static {
-    fn init(self) -> Self;
+
+    /// Creates the Application.
+    fn init(self, configuration: &WGPUConfiguration) -> Self;
+
+    /// The render function for application.
     fn render(self);
+
+    /// A function that handles inputs.
     fn input(self);
+
+    /// A function for resizing.
     fn resize(self);
+
+    /// A function for updating the state of the application.
     fn update(self);
 }
 
-pub trait Loop {
-    fn run<A: Application>(configuration: WGPUConfiguration);
+/// A trait for Loops.
+pub trait Loop: Sized + 'static {
+
+    /// Run function that starts the loop. Beware: run takes ownership of application and
+    /// configuration.
+    fn run<A: Application>(self, application: A, configuration: WGPUConfiguration);
 }
 
-/// A struct that holds wgpu-rs resources.
+/// A basic loop.
+pub struct BasicLoop { }
+
+impl Loop for BasicLoop {
+
+    fn run<A: Application>(self, mut application: A, WGPUConfiguration {
+        window,
+        event_loop,
+        instance,
+        size,
+        surface,
+        adapter,
+        device,
+        queue}: WGPUConfiguration,) {
+
+
+    // Create thread pool and spawner for native version.
+    #[cfg(not(target_arch = "wasm32"))]
+    let (mut pool, _spawner) = {
+
+        let local_pool = futures::executor::LocalPool::new();
+        let spawner = local_pool.spawner();
+        (local_pool, spawner)
+    };
+
+    // Define spawner for wasm version.
+    #[cfg(target_arch = "wasm32")]
+    let spawner = {
+        use futures::{future::LocalFutureObj, task::SpawnError};
+        use winit::platform::web::WindowExtWebSys;
+
+        struct WebSpawner {}
+        impl LocalSpawn for WebSpawner {
+            fn spawn_local_obj(
+                &self,
+                future: LocalFutureObj<'static, ()>,
+            ) -> Result<(), SpawnError> {
+                Ok(wasm_bindgen_futures::spawn_local(future))
+            }
+        }
+
+        std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+
+        // On wasm, append the canvas to the document body
+        web_sys::window()
+            .and_then(|win| win.document())
+            .and_then(|doc| doc.body())
+            .and_then(|body| {
+                body.append_child(&web_sys::Element::from(window.canvas()))
+                    .ok()
+            })
+            .expect("couldn't append canvas to document body");
+
+        WebSpawner {}
+    };
+
+    // Launch the loop.
+    event_loop.run(move |event, _, control_flow| {
+
+        // Move the ownerships to this function.
+        let _ = (&window,
+                &instance,
+                &size,
+                &surface,
+                &adapter,
+                &device,
+                &queue,
+                &mut application);
+
+        *control_flow = ControlFlow::Poll;
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            pool.run_until_stalled();
+        }
+
+        match event {
+
+            Event::MainEventsCleared => {
+                #[cfg(not(target_arch = "wasm32"))]
+                {
+                    pool.run_until_stalled();
+                }
+
+                #[cfg(target_arch = "wasm32")]
+                window.request_redraw();
+            }
+            Event::WindowEvent { event: WindowEvent::Resized(size), .. } => {
+                //state.resize(size);
+            }
+            Event::WindowEvent {event, .. } => {
+                //if state.input(&event) { /* state.update() */ }
+                match event {
+                    WindowEvent::CloseRequested => {
+                        *control_flow = ControlFlow::Exit
+                    }
+                    WindowEvent::KeyboardInput { input, ..  } => {
+                        match input {
+                            KeyboardInput {
+                                state: ElementState::Pressed,
+                                virtual_keycode: Some(VirtualKeyCode::Escape),
+                                ..
+                            } => *control_flow = ControlFlow::Exit,
+                            _ => {}
+                        } // match input
+                    } // KeyboardInput
+                    _ => { /*state.update()*/ } // Other WindowEvents
+                } // match event (WindowEvent)
+            } // Event::WindowEvent
+            Event::RedrawRequested(_) => {
+                //state.render(&window);
+            }
+            _ => { } // Any other events
+        } // match event
+    }); // run
+
+    }
+}
+
+/// A struct that holds the wgpu-rs application resources.
 pub struct WGPUConfiguration {
     pub window: winit::window::Window,
     pub event_loop: EventLoop<()>,
@@ -29,7 +165,7 @@ pub struct WGPUConfiguration {
 }
 
 /// A trait to configure wgpu-rs engine. TODO: Do we need this? 'static + Sized
-pub trait WGPUFeatures {
+pub trait WGPUFeatures: Sized + 'static {
     fn optional_features() -> wgpu::Features {
         wgpu::Features::empty()
     }
@@ -41,8 +177,8 @@ pub trait WGPUFeatures {
     }
 }
 
-/// Initializes wgpu-rs system. TODO: create Result or Option version.
-pub async fn setup<P: WGPUFeatures>(title: &str) -> WGPUConfiguration {
+/// Initializes wgpu-rs system. TODO: finish the Result<...>.
+pub async fn setup<P: WGPUFeatures>(title: &str) -> Result<WGPUConfiguration, &'static str> {
 
     println!("YEEEAAAAH");
 
@@ -77,7 +213,7 @@ pub async fn setup<P: WGPUFeatures>(title: &str) -> WGPUConfiguration {
                 body.append_child(&web_sys::Element::from(window.canvas()))
                     .ok()
             })
-            .expect("couldn't append canvas to document body");
+            .expect("couldn't append canvas to document body.");
     }
 
     log::info!("Initializing the surface...");
@@ -133,14 +269,14 @@ pub async fn setup<P: WGPUFeatures>(title: &str) -> WGPUConfiguration {
         .await
         .unwrap();
 
-    WGPUConfiguration {
-        window: window,
-        event_loop: event_loop,
-        instance: instance,
-        size: size,
-        surface: surface,
-        adapter: adapter,
-        device: device,
-        queue: queue,
-    }
+    Ok(WGPUConfiguration {
+            window: window,
+            event_loop: event_loop,
+            instance: instance,
+            size: size,
+            surface: surface,
+            adapter: adapter,
+            device: device,
+            queue: queue,
+    })
 }
