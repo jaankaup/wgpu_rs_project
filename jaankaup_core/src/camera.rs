df45f59d7a29ab06efd8dbbf01b4b1fd7176f4f9
jaankaup <janne.a.kauppinen@student.jyu.fi>
2021-01-23 16:31:14 +0200

    Removed CameraController from the project.

--- jaankaup_core/src/camera.rs
+++ jaankaup_core/src/camera.rs
@@ -33,7 +33,30 @@ pub struct RayCamera {
 unsafe impl bytemuck::Zeroable for RayCamera {}
 unsafe impl bytemuck::Pod for RayCamera {}
 
-///////////////////////////////////////////////////////////////////////////////////////
+/// Struct that represent uniform data in shader.
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct CameraUniform {
+    view_proj: cgmath::Matrix4<f32>,
+    pos: cgmath::Vector4<f32>,
+}
+
+impl CameraUniform {
+    pub fn new() -> Self {
+        Self {
+            view_proj: cgmath::Matrix4::identity(),
+            pos: cgmath::Vector4::new(1.0,1.0,1.0,1.0),
+        }
+    }
+
+    pub fn update_view_proj(&mut self, camera: &Camera) {
+        self.view_proj = camera.build_projection_matrix();
+        self.pos = Vector4::new(camera.pos.x, camera.pos.y, camera.pos.z, 1.0);
+    }
+}
+ 
+unsafe impl bytemuck::Zeroable for CameraUniform {}
+unsafe impl bytemuck::Pod for CameraUniform {}
 
 /// A camera for basic rendering.
 #[repr(C)]
@@ -96,10 +119,6 @@ impl Camera {
         // Convert time delta to milli seconds.
         let time_delta_milli_f32 = time_delta_nanos as f32 / 1000000.0;
 
-        // println!("time_delta_nanos == {}", time_delta_nanos);
-        // println!("movement == {}", (time_delta_nanos as f32) / 1000000.0);
-        // println!("********************************");
-
         // The right vector.
         let right = self.view.cross(self.up);
 
@@ -115,18 +134,12 @@ impl Camera {
 
         self.pos += movement;
 
-        //println!("movement == ({}, {}, {})", movement.x, movement.y,movement.z);
 
-
-        // match left_mouse_button {
-        //     Some(InputState::Pressed(_)) => { println!("Left mouse is Pressed.") } ,
-        //     Some(InputState::Down(_,_)) => { println!("Left mouse is Down.") } ,
-        //     Some(InputState::Released(_, _)) => { println!("Left mouse is Released.") } ,
-        //     None => { println!("Left mouse is None.") } ,
-        // }
-        
-        let md = input_cache.get_mouse_delta();
         // Rotation.
+          
+        let md = input_cache.get_mouse_delta();
+
+        // If left mouse is down update pitch, yaw and view.
         if let Some(InputState::Down(_,_)) = left_mouse_button {
 
             self.pitch = clamp(
@@ -140,9 +153,6 @@ impl Camera {
                 self.pitch.to_radians().cos() * self.yaw.to_radians().sin()
             ).normalize_to(1.0);
         }
-
-        // println!("camera position == ({}, {}, {})", self.pos.x, self.pos.y, self.pos.z);
-        // println!("camera view == ({}, {}, {})", self.view.x, self.view.y, self.view.z);
     }
 
     /// Creates a pv matrix for wgpu.
@@ -162,246 +172,6 @@ impl Camera {
     }
 }
 
-///////////////////////////////////////////////////////////////////////////////////////
-
-/// A controller for handling the input and state of camera related operations.
-pub struct CameraController {
-    speed: f32,
-    sensitivity: f32,
-    is_up_pressed: bool,
-    is_down_pressed: bool,
-    is_forward_pressed: bool,
-    is_backward_pressed: bool,
-    is_left_pressed: bool,
-    is_right_pressed: bool,
-    is_left_mouse_pressed: bool,
-    start_mouse_pos: Option<(f64,f64)>,
-    current_mouse_pos: Option<(f64,f64)>,
-    pub pitch: f32,
-    pub yaw: f32,
-}
-
-impl CameraController {
-    pub fn new(speed: f32, sensitivity: f32) -> Self {
-        Self {
-            speed,
-            sensitivity,
-            is_up_pressed: false,
-            is_down_pressed: false,
-            is_forward_pressed: false,
-            is_backward_pressed: false,
-            is_left_pressed: false,
-            is_right_pressed: false,
-            is_left_mouse_pressed: false,
-            start_mouse_pos: Some((0 as f64,0 as f64)),
-            current_mouse_pos: Some((0 as f64,0 as f64)),
-            pitch: -80.5,
-            yaw: -50.0,
-        }
-    }
-
-    pub fn process_events(&mut self, _input_cache: &InputCache) -> bool {
-        true    
-    }
-    
-    //pub fn process_events(&mut self, event: &WindowEvent) -> bool {
-    //    match event {
-    //        WindowEvent::KeyboardInput {
-    //            input:
-    //                KeyboardInput {
-    //                    state,
-    //                    virtual_keycode: Some(keycode),
-    //                    ..
-    //                },
-    //            ..
-    //        } => {
-    //            let is_pressed = *state == ElementState::Pressed;
-    //            let event_happened =
-    //            match keycode {
-    //                VirtualKeyCode::Space => {
-    //                    self.is_up_pressed = is_pressed;
-    //                    true
-    //                }
-    //                VirtualKeyCode::C => {
-    //                    self.is_down_pressed = is_pressed;
-    //                    true
-    //                }
-    //                VirtualKeyCode::W | VirtualKeyCode::Up => {
-    //                    self.is_forward_pressed = is_pressed;
-    //                    true
-    //                }
-    //                VirtualKeyCode::A | VirtualKeyCode::Left => {
-    //                    self.is_left_pressed = is_pressed;
-    //                    true
-    //                }
-    //                VirtualKeyCode::S | VirtualKeyCode::Down => {
-    //                    self.is_backward_pressed = is_pressed;
-    //                    true
-    //                }
-    //                VirtualKeyCode::D | VirtualKeyCode::Right => {
-    //                    self.is_right_pressed = is_pressed;
-    //                    true
-    //                }
-    //                _ => false,
-    //            };
-
-    //            event_happened // TODO: remove
-
-    //        }, // WindowEvent::KeyboardInput
-
-    //        WindowEvent::MouseInput {
-    //                state,
-    //                button,
-    //                ..
-    //        } => { 
-    //            let is_pressed = *state == ElementState::Pressed;
-    //            let event_happened =
-    //            match button {
-    //                MouseButton::Left => {
-    //                    self.is_left_mouse_pressed = is_pressed;
-    //                    true
-    //                }
-    //                _ => false,
-    //            };
-
-    //            event_happened
-    //        }, // WindowEvent::MouseEvent
-
-    //        WindowEvent::CursorMoved {
-    //                position,
-    //                ..
-    //        } => { 
-
-    //            // Initial mouse positions.
-    //            match self.start_mouse_pos {
-    //                Some(_) => { },
-    //                None      => {
-    //                    self.start_mouse_pos = Some((position.x, position.y));
-    //                    self.current_mouse_pos = Some((position.x, position.y));
-    //                },
-    //            }
-
-    //            // Update both previous and current mouse positions.
-    //            self.start_mouse_pos = self.current_mouse_pos;
-    //            self.current_mouse_pos = Some((position.x, position.y));
-
-    //            true
-    //        }, // WindowEvent::CursorMoved
-
-    //        _ => false, // ignore other events
-    //    } // event
-    //} // end func
-
-    pub fn update_camera(&mut self, camera: &mut Camera, input_cache: &InputCache) {
-                                                               
-        let forward = camera.view;
-        //println!("{}", input_cache.key_state(&Key::W) == false);
-
-        let state: Option<InputState> = input_cache.key_state(&Key::W);
-
-        match state {
-            Some(InputState::Down(_,_)) => { println!("Moving forward (down).") }, 
-            Some(InputState::Pressed(_)) => { println!("Moving forward (pressed).") }, 
-            Some(InputState::Released(_,_)) => { println!("Stop moving forward (released)") }, 
-            _ => { println!("Released long time ago.") },
-            //camera.pos += forward * self.speed;
-        }
-         //if let input_cache.key_state(&Key::W) {
-         //    camera.pos += forward * self.speed;
-         //}
-        if self.is_backward_pressed {
-            camera.pos -= forward * self.speed;
-        }
-
-        let right = forward.cross(camera.up);
-
-        if self.is_right_pressed {
-            camera.pos += right * self.speed;
-        }
-        if self.is_left_pressed {
-            camera.pos -= right * self.speed;
-        }
-        if self.is_up_pressed {
-            camera.pos += camera.up * self.speed;
-        }
-        if self.is_down_pressed {
-            camera.pos -= camera.up * self.speed;
-        }
-        if self.is_left_mouse_pressed {
-            // Update mouse delta.
-            let (x0, y0) = self.start_mouse_pos.unwrap();
-            let (x1, y1) = self.current_mouse_pos.unwrap();
-            let (x,y) = (x1 - x0, y1 - y0); 
-
-            self.pitch = clamp(self.pitch + (self.sensitivity as f32 * (y * (-1.0)) as f32) , -89.0,89.0);
-            //self.pitch = clamp(self.pitch + (self.sensitivity as f32 * (y * (-1.0)) as f32) , -89.0,89.0);
-            self.yaw = self.yaw + self.sensitivity * x as f32 ;
-
-            // println!("yaw/pitch = ({},{})", self.yaw, self.pitch);
-
-            camera.view = Vector3::new(
-                self.pitch.to_radians().cos() * self.yaw.to_radians().cos(),
-                self.pitch.to_radians().sin(),
-                self.pitch.to_radians().cos() * self.yaw.to_radians().sin()
-            ).normalize_to(1.0);
-
-            // println!("view = ({},{},{})", camera.view.x, camera.view.y, camera.view.z);
-
-        }
-    }
-
-    //TODO: refactor.
-    pub fn update_ray_camera(&mut self, camera: &mut RayCamera) {
-                                                               
-        let forward = camera.view;
-
-        if self.is_forward_pressed {
-            camera.pos += forward * self.speed;
-        }
-        if self.is_backward_pressed {
-            camera.pos -= forward * self.speed;
-        }
-
-        let right = forward.cross(camera.up);
-
-        if self.is_right_pressed {
-            camera.pos += right * self.speed;
-        }
-        if self.is_left_pressed {
-            camera.pos -= right * self.speed;
-        }
-        if self.is_up_pressed {
-            camera.pos += camera.up * self.speed;
-        }
-        if self.is_down_pressed {
-            camera.pos -= camera.up * self.speed;
-        }
-        if self.is_left_mouse_pressed {
-            // Update mouse delta.
-            let (x0, y0) = self.start_mouse_pos.unwrap();
-            let (x1, y1) = self.current_mouse_pos.unwrap();
-            let (x,y) = (x1 - x0, y1 - y0); 
-
-            self.pitch = clamp(self.pitch + (self.sensitivity as f32 * (y * (-1.0)) as f32) , -89.0,89.0);
-            self.yaw = self.yaw + self.sensitivity * x as f32 ;
-
-            // println!("yaw/pitch = ({},{})", self.yaw, self.pitch);
-
-            camera.view = Vector3::new(
-                self.pitch.to_radians().cos() * self.yaw.to_radians().cos(),
-                self.pitch.to_radians().sin(),
-                self.pitch.to_radians().cos() * self.yaw.to_radians().sin()
-            ).normalize_to(1.0);
-
-            // println!("ray_view = ({},{},{})", camera.view.x, camera.view.y, camera.view.z);
-
-        }
-    }
-}
-
-///////////////////////////////////////////////////////////////////////////////////////
-
-///// TODO: remove this. Add to the RayCamera. 
 #[repr(C)]
 #[derive(Copy, Clone)]
 pub struct RayCameraUniform {
@@ -443,27 +213,3 @@ unsafe impl bytemuck::Pod for RayCameraUniform {}
 
 ///////////////////////////////////////////////////////////////////////////////////////
 
-/// Camera uniform data for the shader.
-#[repr(C)]
-#[derive(Copy, Clone)]
-pub struct CameraUniform {
-    view_proj: cgmath::Matrix4<f32>,
-    pos: cgmath::Vector4<f32>,
-}
-
-impl CameraUniform {
-    pub fn new() -> Self {
-        Self {
-            view_proj: cgmath::Matrix4::identity(),
-            pos: cgmath::Vector4::new(1.0,1.0,1.0,1.0),
-        }
-    }
-
-    pub fn update_view_proj(&mut self, camera: &Camera) {
-        self.view_proj = camera.build_projection_matrix();
-        self.pos = Vector4::new(camera.pos.x, camera.pos.y, camera.pos.z, 1.0);
-    }
-}
- 
-unsafe impl bytemuck::Zeroable for CameraUniform {}
-unsafe impl bytemuck::Pod for CameraUniform {}
