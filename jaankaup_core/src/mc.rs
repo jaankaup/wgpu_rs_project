dd7fc10c69e845e75161bbc373531f93efc47d3a
jaankaup <janne.a.kauppinen@student.jyu.fi>
2021-01-26 13:14:18 +0200

    Marching cubes should work now (in theory).

--- jaankaup_core/src/mc.rs
+++ jaankaup_core/src/mc.rs
@@ -44,6 +44,10 @@ impl McParams {
         }
     }
 
+    pub fn get_uniform_buffer(&self) -> &wgpu::Buffer {
+        &self.buffer
+    }
+
     pub fn get_params(&self) -> &McUniform {
         &self.params
     }
@@ -78,10 +82,12 @@ impl McParams {
 }
 
 /// A struct for marching cubes algorithm purposes.
+/// Includes the counter buffer. Should it be 'a part of' or outside MarchingCubes?
 pub struct MarchingCubes {
+    // The mc pipeline.
     pipeline: wgpu::ComputePipeline,
-    counter_buffer: Option<wgpu::Buffer>,
-    density_buffer: Option<wgpu::Buffer>,
+    // Counter buffer for mc.
+    counter_buffer: wgpu::Buffer,
 }
 
 impl MarchingCubes {
@@ -89,13 +95,79 @@ impl MarchingCubes {
     pub fn init(device: &wgpu::Device, mc_shader: &wgpu::ShaderModule) -> Self {
         Self {
             pipeline: MarchingCubes::create_pipeline(&device, &mc_shader),
-            counter_buffer: None,
-            density_buffer: None,
+            counter_buffer: buffer_from_data::<u32>(
+                &device,
+                &[0 as u32],
+                wgpu::BufferUsage::STORAGE | wgpu::BufferUsage::COPY_DST |wgpu::BufferUsage::COPY_SRC,
+                None),
         }
     }
 
-//    pub fn create_bind_groups(
-//        device: &device,
+    /// Creates bind groups for marching cubes.
+    pub fn create_bind_groups(
+        &self,
+        device: &wgpu::Device,
+        params: &McParams,
+        output_buffer: &wgpu::Buffer)
+        -> Vec<wgpu::BindGroup> {
+
+        let bind_group_layouts = MarchingCubes::get_bind_group_layout(&device);
+
+        // Create bindings. Group 0.
+        let mc_param_buffer = wgpu::BindGroupEntry {
+                binding: 0,
+                resource: wgpu::BindingResource::Buffer {
+                    buffer: &params.get_uniform_buffer(),
+                    offset: 0,
+                    size: None, //wgpu::BufferSize::new(std::mem::size_of::<McParams>(),
+                },
+        };
+
+        let mc_counter_buffer = wgpu::BindGroupEntry {
+                binding: 1,
+                resource: wgpu::BindingResource::Buffer {
+                    buffer: &self.counter_buffer,    
+                    offset: 0,
+                    size: None, //wgpu::BufferSize::new(std::mem::size_of::<u32>()),
+                },
+        };
+
+        // Create bindings. Group 1.
+        let mc_output_buffer = wgpu::BindGroupEntry {
+                binding: 0,
+                resource: wgpu::BindingResource::Buffer {
+                    buffer: &output_buffer,    
+                    offset: 0,
+                    size: None, //wgpu::BufferSize::new(std::mem::size_of::<u32>(),
+                },
+        };
+
+        let bind_group_0 = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &bind_group_layouts[0],
+            entries: &[mc_param_buffer, mc_counter_buffer],
+            label: None,
+        });
+
+        let bind_group_1 = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &bind_group_layouts[1],
+            entries: &[mc_output_buffer],
+            label: None,
+        });
+
+        vec![bind_group_0, bind_group_1]
+    }
+
+    pub fn dispatch(&self, bind_groups: &Vec<wgpu::BindGroup>, encoder: &mut wgpu::CommandEncoder, x: u32, y: u32, z: u32) {
+
+        let mut pass = encoder.begin_compute_pass(
+            &wgpu::ComputePassDescriptor { label: None}
+        );
+        pass.set_pipeline(&self.pipeline);
+        for (e, bgs) in bind_groups.iter().enumerate() {
+            pass.set_bind_group(e as u32, &bgs, &[]);
+        }
+        pass.dispatch(x, y, z)
+    }
 
     /// Creates the BindGroupLayout for marching cubes pipeline and bindgroup.
     fn get_bind_group_layout(device: &wgpu::Device) -> Vec<wgpu::BindGroupLayout> {
