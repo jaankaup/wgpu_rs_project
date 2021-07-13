0f7dac0919d274652c4f77bcc20514a38ed03b4f
jaankaup <janne.a.kauppinen@student.jyu.fi>
2021-07-13 15:21:17 +0300

    Created local coordinate to offset indexing.

--- index_tables/src/lib.rs
+++ index_tables/src/lib.rs
@@ -36,7 +36,6 @@ pub fn create_fmm_index_table(
     for i in 0..block_size as i32  {
         let coordinate = work_x_index_to_uvec3(i as u32, x_dim as u32, y_dim as u32, z_dim as u32);
         result.push([coordinate[0], coordinate[1], coordinate[2], i]);
-        //result.push(i);
     }
 
     // Compute the ghost zone data.
@@ -46,37 +45,31 @@ pub fn create_fmm_index_table(
             0 => { 
                 let coord = work_x_index_to_uvec3(j as u32, x_dim as u32, y_dim as u32, z_dim as u32);
                 result.push([coord[0], coord[1], coord[2]-1, index_offset_z + j as i32]);
-                //result.push(index_offset_z + j); // Front neighbors (x,y,-z)
             }
             1 => { 
                 let coord = work_x_index_to_uvec3((j + x_dim*y_dim * 3) as u32, x_dim as u32, y_dim as u32, z_dim as u32);
                 result.push([coord[0], coord[1], coord[2]+1, -index_offset_z + j as i32]);
-                //result.push(-index_offset_z + j); // Back neighbors
             }
             2 => {
                 let index = ((x_dim - 1) * (j+1)) as i32;
                 let coord = work_x_index_to_uvec3(index as u32, x_dim as u32, y_dim as u32, z_dim as u32);
                 result.push([coord[0]+1, coord[1], coord[2], index_offset_x + index]);
-                //result.push(index_offset_x + (x_dim - 1) * (j+1)); // Right neighbors
             }
             3 => { 
                 let index = (x_dim * j) as i32;
                 let coord = work_x_index_to_uvec3(index as u32, x_dim as u32, y_dim as u32, z_dim as u32);
                 result.push([coord[0]-1, coord[1], coord[2], -index_offset_x + index]);
-                //result.push(-index_offset_x + x_dim * j); // Left neighbors
             }
             4 => {
                 let index = (j % y_dim + (j / y_dim) * x_dim*y_dim) as i32;
                 let coord = work_x_index_to_uvec3(index as u32, x_dim as u32, y_dim as u32, z_dim as u32);
                 result.push([coord[0], coord[1]-1, coord[2], -index_offset_y + index]);
-                //result.push(-index_offset_y + j % y_dim + (j / y_dim) * x_dim*y_dim); // Bottom neighbors
             }
 
             5 => {
                 let index = (x_dim * y_dim - x_dim + j % y_dim + (j / y_dim) * x_dim*y_dim) as i32;
                 let coord = work_x_index_to_uvec3(index as u32, x_dim as u32, y_dim as u32, z_dim as u32);
                 result.push([coord[0], coord[1]+1, coord[2], index_offset_y + index]);
-                //result.push(index_offset_y + x_dim * y_dim - x_dim + j % y_dim + (j / y_dim) * x_dim*y_dim); // Top neighbors
             }
             _ => { result.push([66666,66666,666666,666666]); }
         }
@@ -86,25 +79,58 @@ pub fn create_fmm_index_table(
         println!("{:?}",result[i]);
     }
 
-    for i in 0..result.len() as usize {
-        print!("{:?} => ",i);
-        println!("{:?}",reverse_index(&result[i], x_dim, y_dim, z_dim, &result));
-    }
+    // for i in 0..result.len() as usize {
+    //     print!("{:?} => ",i);
+    //     println!("{:?}",reverse_index(&result[i], x_dim, y_dim, z_dim, &result));
+    // }
 
     println!("result_len == {:?}",result.len());
     result
 }
 
-pub fn reverse_index(coordinate: &[i32;4], x_dim: u32, y_dim: u32, z_dim: u32, hash_table: &Vec<[i32;4]>) -> u32 {
+// Create offset table depending on global and local dimensions.
+// TODO: implement local hashing on GPU code.
+pub fn create_hash_table(x_dim: u32,
+                         y_dim: u32,
+                         z_dim: u32,
+                         global_dim_x: u32,
+                         global_dim_y: u32,
+                         global_dim_z: u32) -> Vec<i32> {
 
-    //println!("{:?}",coordinate);
+    let table = create_fmm_index_table(x_dim,y_dim,z_dim,global_dim_x,global_dim_y,global_dim_z);
+    let mut table_2 = table.to_vec().into_iter();
+
     let x_offset = x_dim + 2; 
     let y_offset = y_dim + 2; 
-    
-    let c = [(coordinate[0] + 1) as u32, (coordinate[1] + 1) as u32, (coordinate[2] + 1) as u32];
+    let z_offset = z_dim + 2; 
 
-    //println!("c[0] + c[1] * x_offset + c[2] * x_offset * y_offset == {:?} + {:?} * {:?} + {:?} * {:?} * {:?}",c[0], c[1], x_offset, c[2], x_offset, y_offset);
+    let mut mapping: Vec<i32> = vec![0 ; x_offset as usize * y_offset as usize * z_offset as usize]; // [0 as i32 ; 216];
+
+    for coordinate in table.iter() {
+
+        let c = [(coordinate[0] + 1) as u32, (coordinate[1] + 1) as u32, (coordinate[2] + 1) as u32];
+        let hash_value = c[0] + c[1] * x_offset + c[2] * x_offset * y_offset;
+        let result = table_2.find(|&x| x[0] == coordinate[0] && x[1] == coordinate[1] && x[2] == coordinate[2]).unwrap(); 
+        mapping[hash_value as usize] = result[3]; 
+    }
+
+    mapping
+}
+
+pub fn reverse_index(coordinate: &[i32;4], x_dim: u32, y_dim: u32, z_dim: u32, hash_table: &Vec<[i32;4]>) -> u32 {
+
+    let x_offset = x_dim + 2; 
+    let y_offset = y_dim + 2; 
+
+    let mut mapping = [0 as i32 ; 216];
+
+    let c = [(coordinate[0] + 1) as u32, (coordinate[1] + 1) as u32, (coordinate[2] + 1) as u32];
     let hash_value = c[0] + c[1] * x_offset + c[2] * x_offset * y_offset;
+
+    let mut into_iter = hash_table.into_iter();
+
+    let result = into_iter.find(|&x| x[0] == coordinate[0] && x[1] == coordinate[1] && x[2] == coordinate[2]).unwrap(); 
+    mapping[hash_value as usize] = result[3]; 
 
     hash_value
 }
